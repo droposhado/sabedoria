2f5dfc9e171ff448c4a5b0d52b71e211368ff0e9
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-09-30 03:13:14 +0000

    Update db.py

--- sabedoria/models/db.py
+++ sabedoria/models/db.py
@@ -1,3 +1,30 @@
 import flask_sqlalchemy
 
 db = flask_sqlalchemy.SQLAlchemy()
+
+class Base(db.Model):
+    """Base class with common methods"""
+
+    __abstract__ = True
+
+    id = db.Column(UUID(as_uuid=True),
+                primary_key=True,
+                unique=True,
+                server_default=text("gen_random_uuid()"),)
+
+
+    def save(self):
+        """Create new item"""
+
+        # But need save to update, but exists
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+        db.session.flush()
+
+
+    def delete(self):
+        """Delete item"""
+
+        db.session.delete(self)
+        db.session.commit()
