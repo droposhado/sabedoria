17d5c187d715efc7801bc8d0bc7c55ce53cec0a4
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-09-30 03:13:49 +0000

    Update health.py

--- sabedoria/models/health.py
+++ sabedoria/models/health.py
@@ -4,34 +4,6 @@ from sqlalchemy.dialects.postgresql import UUID
 from .db import db
 
 
-class Base(db.Model):
-    """Base class with common methods"""
-
-    __abstract__ = True
-
-    id = db.Column(UUID(as_uuid=True),
-                primary_key=True,
-                unique=True,
-                server_default=text("gen_random_uuid()"),)
-
-
-    def save(self):
-        """Create new item"""
-
-        # But need save to update, but exists
-        if not self.id:
-            db.session.add(self)
-        db.session.commit()
-        db.session.flush()
-
-
-    def delete(self):
-        """Delete item"""
-
-        db.session.delete(self)
-        db.session.commit()
-
-
 class Liquid(Base):
     """Represent database model liquid"""
 
