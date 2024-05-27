3f7e20d4c52778c9947bfac49a7adb0b5cdcce8e
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-05-27 12:48:58 -0300

    fix reverse url in tests to health routes

--- tests/test_view_health.py
+++ tests/test_view_health.py
@@ -8,7 +8,7 @@ class ApiV1(Base):
     def test_health_get(self):
 
         with self.app.test_request_context():
-            health = self.client.get(url_for("health_method_get"))
+            health = self.client.get(url_for("v2.health_method_get"))
             self.assertEqual(health.status_code, 200)
             self.assertEqual(health.text, "OK")
 
@@ -16,6 +16,6 @@ class ApiV1(Base):
     def test_health_head(self):
 
         with self.app.test_request_context():
-            health = self.client.head(url_for("health_method_head"))
+            health = self.client.head(url_for("v2.health_method_head"))
             self.assertEqual(health.status_code, 200)
             self.assertEqual(health.text, "")
